c530b2b4facc8db697e8b340bdb0aad10c51ea06
kingkar <kingkarbhowmick2@gmail.com>
2025-10-22 18:56:49 +0200

    Dynamic Function Added

--- library-app/script.js
+++ library-app/script.js
@@ -1,9 +1,7 @@
 const myLibrary = [];
 
 
-function Book(){
-  
-}
+
 
 function Book(name, author, title, pagesTotal, isRead,id) {
   // the constructor...
@@ -28,19 +26,62 @@ function addBookToLibrary(name, author, title, pagesTotal, isRead) {
 
 //Write a function that loops through the array and displays each book on the page.
 function display(){
-  for ( let book of myLibrary ){
+  /*or ( let book of myLibrary ){
     console.log("The Properties of this Book are: " +"Name: " + book.name + " , Author: " + book.author + ", Title: " + book.title + ", Pages Number: " + book.pagesTotal + ", Book Read: " + book.isRead + ", Book ID:  " + book.id);
+  }*/
+
+
+  //first target the container from html and then clear
+const libraryContainer =  document.getElementById("library");
+libraryContainer.innerHTML = "";
+
+//Then loop through all books and save to a div element and at last append to the library 
+  for(let books of myLibrary){
+    const element = document.createElement("div");
+    element.classList.add("book-card");
+    element.textContent =  `Name: ${books.name}, Author: ${books.author}, Title: ${books.title}, Pages: ${books.pagesTotal}, Read: ${books.isRead}, ID: ${books.id}`;
+    libraryContainer.appendChild(element);
   }
+
+
 }
+
+// same event listener function liike java
+const form = document.getElementById("bookform");
+
+form.addEventListener("submit", function(event){
+
+  event.preventDefault();
+
+
+  const name = document.getElementById("name").value;
+  const author = document.getElementById("author").value;
+   const title = document.getElementById("title").value;
+   const pagesTotal = document.getElementById("pages").value;
+   //By Radio button which is checked gives you the value, with querySelector
+    const isRead = document.querySelector('input[name="isRead"]:checked').value
+
+  // add to library
+addBookToLibrary(name, author, title, pagesTotal, isRead);
+
+display();
+
+form.reset();
+
+});
+
 
 function deleteLast(){
-  myLibrary.pop();
+myLibrary.pop();
+display();
 }
+  
 
 
-addBookToLibrary("Tim","COOKAuthor","The Blue Sea", 560, true);
+
+/*addBookToLibrary("Tim","COOKAuthor","The Blue Sea", 560, true);
 
 addBookToLibrary("Jones","TomCathor","The Blue Ocean", 560, true);
 
-display();
+display();*/
 
