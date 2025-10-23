e44a9c8f000aca4e03d16ecddd68bc1b2e583980
kingkar <kingkarbhowmick2@gmail.com>
2025-10-23 16:15:46 +0200

    remove button added and html visually improved

--- library-app/script.js
+++ library-app/script.js
@@ -1,3 +1,5 @@
+
+//Our Storage or DB
 const myLibrary = [];
 
 
@@ -24,27 +26,9 @@ function addBookToLibrary(name, author, title, pagesTotal, isRead) {
   
 }
 
-//Write a function that loops through the array and displays each book on the page.
-function display(){
-  /*or ( let book of myLibrary ){
-    console.log("The Properties of this Book are: " +"Name: " + book.name + " , Author: " + book.author + ", Title: " + book.title + ", Pages Number: " + book.pagesTotal + ", Book Read: " + book.isRead + ", Book ID:  " + book.id);
-  }*/
 
 
-  //first target the container from html and then clear
-const libraryContainer =  document.getElementById("library");
-libraryContainer.innerHTML = "";
 
-//Then loop through all books and save to a div element and at last append to the library 
-  for(let books of myLibrary){
-    const element = document.createElement("div");
-    element.classList.add("book-card");
-    element.textContent =  `Name: ${books.name}, Author: ${books.author}, Title: ${books.title}, Pages: ${books.pagesTotal}, Read: ${books.isRead}, ID: ${books.id}`;
-    libraryContainer.appendChild(element);
-  }
-
-
-}
 
 // same event listener function liike java
 const form = document.getElementById("bookform");
@@ -71,11 +55,72 @@ form.reset();
 });
 
 
-function deleteLast(){
-myLibrary.pop();
-display();
+
+//Write a function that loops through the array and displays each book on the page.
+function display(){
+  /*or ( let book of myLibrary ){
+    console.log("The Properties of this Book are: " +"Name: " + book.name + " , Author: " + book.author + ", Title: " + book.title + ", Pages Number: " + book.pagesTotal + ", Book Read: " + book.isRead + ", Book ID:  " + book.id);
+  }*/
+
+
+  //first target the container from html and then clear
+const libraryContainer =  document.getElementById("library");
+libraryContainer.innerHTML = "";
+
+//Then loop through all books and save to a div element and at last append to the library 
+  for(let books of myLibrary){
+    const element = document.createElement("div");
+    element.classList.add("book-card");
+    element.innerHTML =  `
+    <p><strong>Name of Book:</strong> ${books.name}</p>
+    <p><strong>Author:</strong> ${books.author}</p> 
+    <p><strong>Title or Subtitle:</strong>  ${books.title}</p>
+     <p><strong>Pages: </strong> ${books.pagesTotal}</p>
+     <p><strong>Read:</strong> ${books.isRead}</p>
+     <button class="delete-btn" id="${books.id}">Delete</button>
+     `;
+  libraryContainer.appendChild(element);
 }
-  
+
+
+     }
+
+
+
+
+const deleteButtons = document.getElementsByClassName("delete-btn")
+
+//loop through all delete buttons to add a event listener to each button
+for( let i = 0 ; i < deleteButtons.length ; i++){
+
+  deleteButtons[i].addEventListener("click", function(event){
+    const bookId = event.target.id;
+
+    //call delete function and pass the particular bookId
+    deleteBook(bookId);
+  });
+}
+
+function deleteBook(bookId){
+
+  // Find the index of the book in our array 
+
+  for ( let i = 0 ; i <= myLibrary.length ; i++){
+
+    if(myLibrary[i].id === bookId) {
+        //remove 1 element  at postion i 
+        myLibrary.splice(i , 1);
+        break;
+    }
+  }
+
+  display();
+}
+
+
+
+
+
 
 
 
